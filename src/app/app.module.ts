4d7ad638929d161f235ee0ddbbd305651bc99ef7
danielreichler <dr@reichler.online>
2020-03-21 15:07:28 +0100

    Added Docker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {environment} from '../environments/environment';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -16,7 +18,9 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {provide: 'BACKEND_API_URL', useValue: environment.api.baseUrl},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
