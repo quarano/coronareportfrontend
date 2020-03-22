2a6270be20c97a6a5e9b2bf86aac9ede48d89e96
Rudolf Biczok <rcbiczok@gmail.com>
2020-03-22 14:12:41 +0100

    Fix complilation errors

--- src/app/contact/contact-entry/contact-entry.component.ts
+++ src/app/contact/contact-entry/contact-entry.component.ts
@@ -1,21 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
-
-
 import { SnackbarService } from './../../services/snackbar.service';
 import { Subscription } from 'rxjs';
 import { ApiService } from './../../services/api.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { DiaryEntry } from 'src/app/models/diary-entry';
+import { DiaryEntryDto } from 'src/app/models/diary-entry';
 import { ActivatedRoute } from '@angular/router';
-import { Symptom } from 'src/app/models/symptom';
+import { SymptomDto } from 'src/app/models/symptom';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 import { MatSelectChange } from '@angular/material/select';
 
-
-
-
+export interface Tile {
+  text: string;
+  cols: number;
+  rows: number;
+  color: string;
+ }
 
 @Component({
   selector: 'app-contact-entry',
