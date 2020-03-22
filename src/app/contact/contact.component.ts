858ef4a2e571016eefc8213288b6532fa0c93487
Leo <leonhard.rassl@gmail.com>
2020-03-22 02:09:15 +0100

    Update contact.component.ts

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  typesOfShoes: string[] = ['CoronaFreund1', 'CoronaFreund2', 'CoronaFreund3', 'CoronaFreund4', 'CoronaFreund5'];
+
   constructor() { }
 
   ngOnInit() {
