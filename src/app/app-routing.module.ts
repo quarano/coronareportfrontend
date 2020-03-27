145578888d7f84d8488e600a71753d2b705b3174
danielreichler <dr@reichler.online>
2020-03-27 01:42:10 +0100

    prototype report

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { IsAuthenticatedGuard } from './guards/is-authenticated.guard';
 import { IsNotAuthenticatedGuard } from './guards/is-not-authenticated.guard';
+import {IsTenantAdminGuard} from './guards/is-tenant-admin.guard';
 
 const routes: Routes = [
   {
     path: 'welcome', loadChildren: () =>
-      import('./welcome/welcome.module').then(m => m.WelcomeModule), canLoad: [IsNotAuthenticatedGuard]
+      import('./welcome/welcome.module').then(m => m.WelcomeModule), canActivate: [IsNotAuthenticatedGuard]
   },
   {
     path: 'diary', loadChildren: () =>
-      import('./diary/diary.module').then(m => m.DiaryModule), canLoad: [IsAuthenticatedGuard]
+      import('./diary/diary.module').then(m => m.DiaryModule), canActivate: [IsAuthenticatedGuard]
   },
   {
     path: 'contacts', loadChildren: () =>
-      import('./contact/contact.module').then(m => m.ContactModule), canLoad: [IsAuthenticatedGuard]
+      import('./contact/contact.module').then(m => m.ContactModule), canActivate: [IsAuthenticatedGuard]
+  },
+  {
+    path: 'tenant-admin', loadChildren: () =>
+      import('./tenant-admin/tenant-admin.module').then(m => m.TenantAdminModule)
   },
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   api: {
-    baseUrl: 'http://localhost:3000'
+    baseUrl: 'https://api-dev.epitrack.de'
   }
 };
 
