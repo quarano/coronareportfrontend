57b3299b9b1489e8b6d85a2b0b21583259014ca5
Mira Manger <mira.manger@gmx.de>
2020-03-28 14:44:05 +0100

    moved header to separate component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,15 +11,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public progressBarActive$$ = this.progressBarService.progressBarActive$$;
-  public isAuthenticated$$ = this.userService.isAuthenticated$$;
-  public urlTenant = this.tenantService.urlTenant;
+
   public tenant$$ = this.tenantService.tenant$$;
-  public tenantsEnum = TenantsEnum;
 
   constructor(
-    private userService: UserService,
-    private progressBarService: ProgressBarService,
     public router: Router,
     private tenantService: TenantService) {
   }
